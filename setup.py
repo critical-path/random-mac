96c139162c215787338541d1ca2c89bee360b2c5
critical-path <37241479+critical-path@users.noreply.github.com>
2019-04-28 00:50:48 -0400

    On branch master  Changes to be committed:
    	new file:   requirements.txt
    	new file:   setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+
+setuptools.setup(
+  name="random-mac",
+  version="0.1.0",
+  description="Use machine learning to identify randomly-generated MAC addresses.",
+  url="https://github.com/critical-path/random-mac",
+  author="critical-path",
+  author_email="n/a",
+  license="MIT",
+  classifiers=[
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3"
+  ],
+  keywords="python machine-learning ml random media-access-control mac mac-address",
+  packages=setuptools.find_packages()
+)
